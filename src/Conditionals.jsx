ebf257616e98b25712b56247a5d4a708d3a6cba4
singhAbhiPNW <sing1290@pnw.edu>
2023-12-09 22:44:15 -0600

    ui fix

--- src/Conditionals.jsx
+++ src/Conditionals.jsx
@@ -331,7 +331,7 @@ function BlogSection() {
 function ShopCollections() {
   return (
     <div className="relative overflow-hidden bg-white">
-      <div className="pb-80 pt-16 sm:pt-24 lg:pt-40">
+      <div className="pb-80 pt-14 sm:pt-20 lg:pt-36">
         <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
           <div className="sm:max-w-lg">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
--- src/Header.jsx
+++ src/Header.jsx
@@ -5,10 +5,12 @@ import { Navbar } from "./Navbar";
 export const Header = () => {
     return (
         <div style={{ width: '100vw', height: '15vh', background: `url("/banner.png")` }}>
-            <div style={{ display: 'flex'}}>
-                <img src="/vite.svg" width={'150'} style={{marginLeft: '5%', }} alt="Logo" />
-                <h2 className="text-4xl font-bold tracking-tight text-white sm:text-5xl" style={{ paddingTop: '2%', color: 'purple'}}>Computational Thinking</h2>
-                <Social/>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <div className="flex items-center" style={{width:'50%'}}>
+                    <img src="/vite.svg" width={'150'} style={{ marginLeft: '10%' }} alt="Logo" />
+                    <h2 className="text-5xl font-bold tracking-tight sm:text-5xl" style={{ color: 'purple'}}>Computational Thinking</h2>
+                </div>
+                <Social />
             </div>
         </div>
     );
@@ -16,13 +18,13 @@ export const Header = () => {
 
 const Social = () => {
     return (
-        <div className="flex" style={{marginLeft:'35%', marginTop:'36px'}}>
-            <img src="/social-icons/facebook.png" style={{width:50, height:50, marginRight:10, cursor:'pointer'}} alt="meta"></img>
-            <img src="/social-icons/instagram.png" style={{width:50, height:50, marginRight:10, cursor:'pointer'}}  alt="instagram"></img>
-            <img src="/social-icons/twitter.png" style={{width:50, height:50, marginRight:10, cursor:'pointer'}} alt="twitter"></img>
-            <img src="/social-icons/linkedin.png" style={{width:50, height:50, marginRight:10, cursor:'pointer'}}  alt="linkedin"></img>
-            <img src="/social-icons/youtube.png" style={{width:50, height:50, marginRight:10, cursor:'pointer'}}  alt="youtube"></img>
-            <img src="/social-icons/whatsapp.png" style={{width:50, height:50, marginRight:10, cursor:'pointer'}}  alt="whatsapp"></img>
+        <div className="flex py-auto" style={{ marginRight: '5%' }}>
+            <img src="/social-icons/facebook.png" style={{ width: 50, height: 50, marginRight: 10, cursor: 'pointer' }} alt="meta"></img>
+            <img src="/social-icons/instagram.png" style={{ width: 50, height: 50, marginRight: 10, cursor: 'pointer' }} alt="instagram"></img>
+            <img src="/social-icons/twitter.png" style={{ width: 50, height: 50, marginRight: 10, cursor: 'pointer' }} alt="twitter"></img>
+            <img src="/social-icons/linkedin.png" style={{ width: 50, height: 50, marginRight: 10, cursor: 'pointer' }} alt="linkedin"></img>
+            <img src="/social-icons/youtube.png" style={{ width: 50, height: 50, marginRight: 10, cursor: 'pointer' }} alt="youtube"></img>
+            <img src="/social-icons/whatsapp.png" style={{ width: 50, height: 50, marginRight: 10, cursor: 'pointer' }} alt="whatsapp"></img>
         </div>
     )
 }
